06889634ce3cfba35972672ca3b8b79d9e5f5a82
Drupus <timmy.burton@hotmail.com>
2021-03-10 14:45:48 -0300

    Mirror weird files + menu remake

--- bomberKPooh/Assets/Scripts/Menu.cs
+++ bomberKPooh/Assets/Scripts/Menu.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Menu : MonoBehaviour
 {
-    public UnityEngine.UI.Button BnStart;
+    public UnityEngine.UI.Button bnStartSolo;
+    public UnityEngine.UI.Button bnQuit;
 
     void Start()
     {
-        BnStart.onClick.AddListener(StartGameSolo);
+        bnStartSolo.onClick.AddListener(StartGameSolo);
+        bnQuit.onClick.AddListener(QuitGame);
     }
 
-    // Update is called once per frame
     void StartGameSolo()
 	{
-
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
 	}
+
+    void QuitGame()
+	{
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
 }
