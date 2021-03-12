c3e7953b2440072f9c3d1dfa543e95fbed9af030
Matias Gultig <magontek@gmail.com>
2021-03-12 00:59:56 -0300

    Tilemap test

--- bomberKPooh/Assets/Scripts/PlayerMove.cs
+++ bomberKPooh/Assets/Scripts/PlayerMove.cs
@@ -18,6 +18,26 @@ public class PlayerMove : MonoBehaviour
     {
         DirX = Input.GetAxis("Horizontal");
         DirY = Input.GetAxis("Vertical");
+        if(DirY!=0)
+        {
+            GetComponent<Animator>().SetInteger("Estado", 1);
+        }
+        else if (DirX != 0)
+        {
+            if (DirX > 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            GetComponent<Animator>().SetInteger("Estado", 2);
+        }
+        else
+        {
+            GetComponent<Animator>().SetInteger("Estado", 0);
+        }
         transform.Translate(new Vector2(DirX, DirY) * Time.deltaTime * PlayerSpeed);        // time.deltatime hace que el movimiento este en referencia al tiempo en vez de los fps
     }
 }
